56bc0c8ea21061a815e2bc56bdfa4294999fe4e3
somadisingh <btech10001.19@bitmesra.ac.in>
2023-12-25 19:38:15 +0530

    initial commit for the backend

---
+++ src/main/java/com/example/kafkalite/KafkaliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkalite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend/kafkalite/KafkaliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend.kafkalite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
