84f534de0e9328a50353c64ff7cecb0a2536210a
somadisingh <btech10001.19@bitmesra.ac.in>
2023-12-26 11:34:26 +0530

    updated placeholder logic to accomodate for multiple placeholders in a template

--- src/shellscripts/start_kafka.bat
+++ src/shellscripts/start_kafka.bat
@@ -4,6 +4,6 @@ cd C:\kafka_2.13-3.6.1
 
 start "Zookeeper Server" cmd /c "bin\windows\zookeeper-server-start.bat config\zookeeper.properties"
 
-timeout /nobreak /t 10 >nul
+timeout /nobreak /t 6 >nul
 
 start "Kafka Server" cmd /c "bin\windows\kafka-server-start.bat config\server.properties"
